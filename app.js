17c4ecf612f386cc6907a868ccef033b197102d6
slingeshlingesh-star <slingeshlingesh@gmail.com>
2026-02-20 15:38:32 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req, res) => {
+    res.send("Hello this my BCA cicd pipeline project");
+});
+
+app.listen(3000, () => {
+    console.log("server running on port 3000");
+});
