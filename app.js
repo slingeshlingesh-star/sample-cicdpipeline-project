ef7370de80bf26cd1154c0ab5c0e33ee35e0d71f
slingeshlingesh-star <slingeshlingesh@gmail.com>
2026-02-20 17:30:04 +0530

    Test CI/CD pipeline

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req, res) => {
-    res.send("Hello this my BCA cicd pipeline project");
+    res.send("Hello this my BCA HELLO WORLD cicd pipeline project");
 });
 
 app.listen(3000, () => {
     console.log("server running on port 3000");
-});
+})
