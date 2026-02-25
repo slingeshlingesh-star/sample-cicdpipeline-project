39ae90aa37a7660d956812a432fc4faeab68ee29
slingeshlingesh-star <slingeshlingesh@gmail.com>
2026-02-25 12:03:13 +0530

    new changesh on code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req, res) => {
-    res.send("Hello this my BCA HELLO WORLD cicd pipeline project");
+    res.send("CICD PIPELINE IS WORKING SUCCESSFULLY ON AWS ECS");
 });
 
 app.listen(3000, () => {
