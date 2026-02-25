07c43acbe57145ca29dcfa71e6824d5013e3ef5c
slingeshlingesh-star <slingeshlingesh@gmail.com>
2026-02-25 17:07:03 +0530

    pipeline test  one

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req, res) => {
-    res.send("CICD PIPELINE IS WORKING SUCCESSFULLY ON AWS ECS");
+    res.send("pipeline test one");
 });
 
 app.listen(3000, () => {
