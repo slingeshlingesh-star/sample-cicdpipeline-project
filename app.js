2b2a56943aabf5ada328d8252b8a3211c4de9e12
slingeshlingesh-star <slingeshlingesh@gmail.com>
2026-02-27 11:52:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req, res) => {
-    res.send("This is a sampel project");
+    res.send("This is a sampel project version");
 });
 
 app.listen(3000, "0.0.0.0", () => {
