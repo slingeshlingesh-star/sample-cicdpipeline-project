22512c1d43f52ab06e52c189877c7a1a0fa3b437
slingeshlingesh-star <slingeshlingesh@gmail.com>
2026-03-16 09:53:54 +0530

    title update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>  YEAR PROJECT </title>
+    <title>  FINAL YAER PROJECT </title>
     <style>
       :root{
         --card-bg: rgba(255,255,255,0.10);
